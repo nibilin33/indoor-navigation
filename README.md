57d5216e4e6d1de92a75949fc150f66817485c3d
nibilin33 <nibilin33@gmail.com>
2023-06-26 21:53:38 +0800

    feat: add amap

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ indoor navigate demo
 ## 高德地图
 
 ## 蜂鸟地图
-https://developer.fengmap.com/develop-jsv3-download.html
+[api docs](https://developer.fengmap.com/develop-jsv3-download.html)
+[preview](https://nibilin33.github.io/indoor-navigation/fengmap.html)       
+
 ## 易景室内三维地图     
 https://developers.weixin.qq.com/community/develop/article/doc/0008aa99a588b870be1932e5451813
 https://www.esmap.cn/docs/sdk3.0/esmap.ESMap.html
--- src/amap/index.js
+++ src/amap/index.js
@@ -1,2 +1,35 @@
-import AMapLoader from "@amap/amap-jsapi-loader";
-
+import Loader from "@amap/amap-jsapi-loader";
+// 加载高德地图JSAPI
+Loader.load({
+  key: "2867cc38b67bdc7b831002d63464ba2c",
+  version: "2.0",
+  plugins: ["AMap.Geocoder", "AMap.IndoorMap", "AMap.IndoorPath"],
+}).then((AMap) => {
+  // todo: Geocoder 方法查不出
+  const lng = 120.3362163838562,
+    lat = 30.308641096219432;
+  // 创建地图实例
+  const map = new AMap.Map("container", {
+    center: [lng, lat],
+    zoom: 18,
+  });
+  const buildingId = "B0G26SKCJP";
+  const floorIds = "B1";
+  const indoorMapLayer = new AMap.IndoorMap(map);
+  // 室内地图无效
+  indoorMapLayer.showIndoorMap(buildingId, floorIds);
+  const clickResult = [];
+  map.on("click", (e) => {
+    const lnglat = e.lnglat; // 获取点击位置的经纬度
+    // const infoWindow = new AMap.InfoWindow({
+    // content: `<div>经度: ${lnglat.lng}</div><div>纬度: ${lnglat.lat}</div>`, // 弹窗内容
+    // position: lnglat, // 弹窗位置
+    // });
+    // infoWindow.open(map);
+    clickResult.push(...[lnglat.lng, lnglat.lat]);
+    const marker = new AMap.Marker({
+      position: [lnglat.lng, lnglat.lat],
+    });
+    map.add(marker);
+  });
+});
